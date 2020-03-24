e5fb22378fc4bd3f0c5063ae0a10e0cfbdff69dc
ThaiTuan123 <53283080+ThaiTuan123@users.noreply.github.com>
2020-03-24 10:45:35 +0700

    Add files via upload

---
+++ Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Dog:Animals
+    {
+        public Dog(string name,string favouriteFood) : base(name, favouriteFood)
+        {
+
+        }
+        public override string ExplainSelf()
+        {
+            Console.WriteLine("DJAAF");
+            return base.ExplainSelf(); ;
+        }
+    }
+}
